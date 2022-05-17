4fa20edf917252fcf55e1cd289747e0c3cc560f1
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-05-17 20:22:11 +0800

    spring-rest

--- src/main/java/com/example/repository/BookRepository.java
+++ src/main/java/com/example/repository/BookRepository.java
@@ -1,9 +1,20 @@
 package com.example.repository;
 
+import java.util.Optional;
+
 import com.example.domain.Book;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface BookRepository extends PagingAndSortingRepository<Book, String> {
+
+    Optional<Book> findBookByIsbn(String isbn);
+
+    @Query("{'title' : { $regex: ?0}}")
+    Iterable<Book> findBookByTitle(String title);
+
+    @Query("{'author': ?0}")
+    Iterable<Book> findBookByAuthor(String author);
 
 }
